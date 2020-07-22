eea3e701045e40ce0ed790da40ca2465e87da57c
Sebastián Vélez <sebastianvelezvelasquez@gmail.com>
2020-07-22 17:20:13 -0500

    Pre production

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 require('./database');
 import express from 'express';
-import data from './data';
 import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
 import cartRoute from './routes/cartRoute';
 import bodyParser from 'body-parser';
 import Product from './models/productModel';
+import path from 'path';
 
 const app = express();
+
 app.use(bodyParser.json())
 app.use("/api/v1/users",userRoute);
 app.use("/api/v1/products",productRoute);
@@ -19,4 +20,14 @@ app.get("/api/v1/categories", async (req,res)=>{
     res.send([...new Set(categories)]);
 });
 
-app.listen(5000,()=>{ console.log("Server Started at port 5000"); });
+//Serve static
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/build'));
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html'));
+    })
+}
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{ console.log(`Server Started at port ${port}`); });
